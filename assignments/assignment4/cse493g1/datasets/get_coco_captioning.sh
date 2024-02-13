dc649fbfa5a37c9ff5d6aa0d0da7a634558dbd48
Sarah Pratt <sarahmhpratt75@gmail.com>
2024-02-13 15:08:23 -0800

    assignment 4

--- assignments/assignment4/cse493g1/datasets/get_coco_captioning.sh
+++
@@ -1,3 +0,0 @@
-wget "https://courses.cs.washington.edu/courses/cse493g1/23au/resources/coco_captioning.zip"
-unzip coco_captioning.zip
-rm coco_captioning.zip
--- assignments/assignment4/cse493g1/datasets/get_imagenet_val.sh
+++
@@ -1,1 +0,0 @@
-wget https://courses.cs.washington.edu/courses/cse493g1/23au/resources/imagenet_val_25.npz
