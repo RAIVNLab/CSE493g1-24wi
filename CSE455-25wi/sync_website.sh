31a4ca2bb6def6da4e627da08b019f54aaf372d7
speedstorm1 <speedstorm123@gmail.com>
2025-01-30 12:18:28 -0800

    add recording 8

--- CSE455-25wi/sync_website.sh
+++ CSE455-25wi/sync_website.sh
@@ -3,4 +3,4 @@
 rsync -avz \
   --exclude .git/ \
   --exclude venv/ \
-   . jjung04@attu.cs.washington.edu:/cse/web/courses/cse455/25wi/.
+   . ayush123@attu.cs.washington.edu:/cse/web/courses/cse455/25wi/.
