ab4878592ec172181e9a50cd14e934e884f2d8e3
nishatakhan <nishat.eee08@gmail.com>
2025-02-28 14:23:36 -0800

    recitation feb28

--- CSE455-25wi/sync_website.sh
+++ CSE455-25wi/sync_website.sh
@@ -3,4 +3,8 @@
 rsync -avz \
   --exclude .git/ \
   --exclude venv/ \
+<<<<<<< Updated upstream
    . jjung04@attu.cs.washington.edu:/cse/web/courses/cse455/25wi/.
+=======
+   . nkhan51@tricycle.cs.washington.edu:/cse/web/courses/cse455/25wi/.
+>>>>>>> Stashed changes
